55f388416552dc868b237cb5f1b90fb1a3fe184d
Anthony Sanchez <anthony@xebix.com>
2021-09-13 15:13:30 -0500

    add endpoint to delete a job post

--- backend/apis/version1/route_jobs.py
+++ backend/apis/version1/route_jobs.py
@@ -4,7 +4,10 @@ from sqlalchemy.orm import Session
 from db.session import get_db
 from db.models.jobs import Job
 from schemas.jobs import JobCreate, ShowJob
-from db.repository.jobs import create_new_job, retreive_job, list_jobs, update_job_by_id
+from db.repository.jobs import (create_new_job,
+        retreive_job, list_jobs,
+        update_job_by_id,
+        delete_job_by_id)
 from typing import List
 
 router = APIRouter()
@@ -41,3 +44,13 @@ def update_job(id:int, job:JobCreate, db:Session=Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
         detail="Job with id {id} does not exist")
     return {"detail": "Successfully updated data."}
+
+
+@router.delete("/delete/{id}")
+def delete_job(id:int, db:Session=Depends(get_db)):
+    owner_id = 1
+    message = delete_job_by_id(id=id, db=db, owner_id=owner_id)
+    if not message:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        detail="Job with id {id} does not exist")
+    return {"detail": "Successfully deleted the job"}
